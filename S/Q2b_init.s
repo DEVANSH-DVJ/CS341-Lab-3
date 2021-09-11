0069016ae51c814c7e7833eaa7314893acdcc8d9
devansh-dvj <devansh.dvj@gmail.com>
2021-09-11 18:20:23 +0530

    Code for Q2b

---
+++ S/Q2b_init.s
@@ -0,0 +1,7 @@
+.text
+
+main:
+    addi s0 zero 5
+    add s1 s0 zero
+    addi s2 zero 6
+    addi s3 zero 7
---
+++ S/Q2b_opt.s
@@ -0,0 +1,7 @@
+.text
+
+main:
+    addi s0 zero 5
+    addi s2 zero 6
+    addi s3 zero 7
+    add s1 s0 zero
