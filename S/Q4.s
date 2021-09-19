f5746821bfc8ad9fdd89b52ead569872f444373e
devansh-dvj <devansh.dvj@gmail.com>
2021-09-19 16:32:14 +0530

    Code for Q4

---
+++ S/Q4.s
@@ -0,0 +1,24 @@
+.text
+
+main:
+    li a0 1
+    li a7 1
+    li a1 2
+    li a6 3
+    add a0 a0 a7
+    li a7 5
+    add a1 a1 a6
+    li a6 8
+    add a0 a0 a7
+    li a7 13
+    add a1 a1 a6
+    li a6 21
+    add a0 a0 a7
+    li a7 34
+    add a1 a1 a6
+    li a6 55
+    add a0 a0 a7
+    li a7 1
+    add a1 a1 a6
+    add a0 a0 a1
+    ecall
